89eb99246a7f115cd0aa637a292d8c406ff30e5b
SYDNEY FRANCIS <110589231+SydneyFrancis@users.noreply.github.com>
2022-10-05 20:08:23 +0530

    try1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,6 +1,31 @@
 package com.driver;
 
 public class Main {
-
-    
+  public static void main(String[] args) {
+    Product p = new Product();
+    int a = p.product(10,15);
+    System.out.println(a);
+    double b = p.product(10.3,2.5);
+    System.out.println(b);
+    int c = p.product(10,20,50);
+    System.out.println(c);
+    System.out.println(a*b*c);
   }
+  static class Product{
+    public Product(){
+
+    }
+    public int product(int x,int y){
+      return x*y;
+    }
+    public int product(int x,int y,int z){
+      return x*y*z;
+    }
+
+    public double product(double x,double y){
+      return  x*y;
+    }
+
+  }
+    
+}
