077284922291521eaf578b8d972da72cf2e1a2db
SYDNEY FRANCIS <110589231+SydneyFrancis@users.noreply.github.com>
2022-10-07 21:46:29 +0530

    DONE

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,27 +1,29 @@
 package com.driver;
 
-public class Main {
-  public static void main(String[] args) {
-    Product p = new Product();
-    System.out.println(p.product(10,15));
-    System.out.println(p.product(10.3,2.5));
-    System.out.println(p.product(10,20,50));
-  }
-  public static class Product{
-    public Product(){
 
-    }
-    public static int product(int x,int y){
+public class Main {
+
+  public static void main(String[] args){
+
+    Product p = new Product();
+    int ans = p.product(1,2);
+    int ans1 = p.product(1,2,3);
+    double ans3 = p.product(1.2,1.4);
+    System.out.println(ans+" "+ans1+" "+ans3);
+  }
+
+  public static class Product{
+
+    public int product(int x, int y) {
+
       return x*y;
     }
-    public static int product(int x,int y,int z){
+    public int product(int x,int y,int z){
       return x*y*z;
     }
-
-    public static double product(double x,double y){
-      return  x*y;
+    public double product(double x, double y) {
+      return x*y;
     }
 
   }
-    
 }
